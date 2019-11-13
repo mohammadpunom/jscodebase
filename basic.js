245f298183cc82f6f4a7e71d5e2dd5aec356340b
mohammadpunom <khyrul@gmail.com>
2019-11-13 15:11:28 +0100

    First Commit

---
+++ basic.js
@@ -0,0 +1,17 @@
+alert("hello");
+alert("world");
+alert(2+3);
+
+var name= prompt("what is your name?");
+alert(name);
+
+var myName = "Khyrul";
+alert(myName);
+var myName123 = "islam";
+alert(myName123);
+var myName_full = "MKIPunom";
+alert(myName_full);
+var myAge = 42;
+alert(myAge);
+
+
