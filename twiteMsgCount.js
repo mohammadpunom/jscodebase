5fb878066af3f190246c1685a64da91d19cc659b
mohammadpunom <khyrul@gmail.com>
2019-11-13 17:49:40 +0100

    third commit

---
+++ twiteMsgCount.js
@@ -0,0 +1,2 @@
+var mymsg = prompt("Please enter your message");
+alert("You are allowed to put a message which is 140 character long." + "\n" + " "+ "You have enter a msg which is " + mymsg.length + " character long." + "\n" + " You have " + (140 - mymsg.length) + " letters remaining.");
